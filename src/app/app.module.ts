0030432a2ae1eb9bda14b232d99de0e220d4e4d9
Alejandro <alecaperez13@gmail.com>
2023-01-27 12:07:16 -0500

    added Cdk drag and drop from material angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,18 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BoardNameComponent } from './components/board-name/board-name.component';
 import { KanbanListComponent } from './components/kanban-list/kanban-list.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     BoardNameComponent,
-    KanbanListComponent
+    KanbanListComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, DragDropModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/kanban-list/kanban-list.component.ts
+++ src/app/components/kanban-list/kanban-list.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-kanban-list',
   templateUrl: './kanban-list.component.html',
-  styleUrls: ['./kanban-list.component.css']
+  styleUrls: ['./kanban-list.component.css'],
 })
 export class KanbanListComponent {
+  ideas = [
+    'Some random ideas',
+    'This is another random idea',
+    'Build an awesome application',
+    'Something to do',
+    'More big ideas',
+    'Too many ideas',
+  ];
+  research = ['Lorem Ipsum', 'Kung fu', 'This must go in the Research column'];
+  todo = [
+    'Get to work',
+    'First get a job',
+    'Fall asleep',
+    'Go home',
+    'Pick up groceries',
+  ];
+  done = ['Get up', 'Brush teeth', 'Take a shower', 'Pet cats'];
 
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
 }
