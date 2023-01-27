a7a9a64c97f01b4c7bd397113df6500ab3b9791e
Alejandro <alecaperez13@gmail.com>
2023-01-27 10:01:00 -0500

    added html and styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BoardNameComponent } from './components/board-name/board-name.component';
+import { KanbanListComponent } from './components/kanban-list/kanban-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BoardNameComponent,
+    KanbanListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/board-name/board-name.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-board-name',
+  templateUrl: './board-name.component.html',
+  styleUrls: ['./board-name.component.css']
+})
+export class BoardNameComponent {
+
+}
